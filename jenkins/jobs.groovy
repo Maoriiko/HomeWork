52972813921db2f169549512ae20405589ed2bad
Maoriiko <maorevar@gmail.com>
2025-08-04 21:51:23 +0300

    Modify 'jobs.groovy' File - Added Build_and_Push_Flask_App and 'Build_and_Push_Nginx_Proxy' PipelineJobs

--- jenkins/jobs.groovy
+++ jenkins/jobs.groovy
@@ -1,4 +1,4 @@
-pipelineJob('Build_and_Run_Flask_Nginx') {
+pipelineJob('Build_and_Push_Flask_App') {
   definition {
     cps {
       script("""
@@ -18,6 +18,11 @@ spec:
     }
   }
 
+  environment {
+    DOCKERHUB_CREDENTIALS = 'dockerhub-credentials-id'
+    DOCKERHUB_REPO = 'maoriiko/flask-app'
+  }
+
   stages {
     stage('Checkout') {
       steps {
@@ -32,23 +37,164 @@ spec:
       }
     }
 
-    stage('Build Flask Image') {
+    stage('Build Docker Image') {
       steps {
         container('docker') {
-          sh 'docker build -t flask-app:latest ./flask-app'
+          sh 'docker build -t \$DOCKERHUB_REPO:latest ./flask-app'
         }
       }
     }
 
-    stage('Build Nginx Image') {
+    stage('Login to DockerHub') {
       steps {
         container('docker') {
-          sh 'docker build -t nginx-proxy:latest ./nginx-proxy'
+          withCredentials([usernamePassword(credentialsId: env.DOCKERHUB_CREDENTIALS, usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+            sh 'echo \$PASS | docker login -u \$USER --password-stdin'
+          }
         }
       }
     }
 
-	stage('Run Containers') {
+    stage('Push Image to DockerHub') {
+      steps {
+        container('docker') {
+          sh 'docker push \$DOCKERHUB_REPO:latest'
+        }
+      }
+    }
+  }
+}
+      """.stripIndent())
+      sandbox()
+    }
+  }
+}
+
+pipelineJob('Build_and_Push_Nginx_Proxy') {
+  definition {
+    cps {
+      script("""
+pipeline {
+  agent {
+    kubernetes {
+      yaml '''
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: docker
+    image: docker:20.10.16-dind
+    securityContext:
+      privileged: true
+'''
+    }
+  }
+
+  environment {
+    DOCKERHUB_CREDENTIALS = 'dockerhub-credentials-id'
+    DOCKERHUB_REPO = 'maoriiko/nginx-proxy'
+  }
+
+  stages {
+    stage('Checkout') {
+      steps {
+        checkout([
+          \$class: 'GitSCM',
+          branches: [[name: '*/main']],
+          userRemoteConfigs: [[
+            url: 'https://github.com/Maoriiko/HomeWork.git',
+            credentialsId: 'GithubToken2'
+          ]]
+        ])
+      }
+    }
+
+    stage('Prepare Nginx Config') {
+      steps {
+        container('docker') {
+          sh '''
+            cp ./nginx-proxy/nginx.conf ./nginx-proxy/nginx.conf.modified
+            sed -i "/location \\\//a \\
+                proxy_set_header X-Forwarded-For \$remote_addr; \\
+                proxy_pass http://flask-app:5000;" ./nginx-proxy/nginx.conf.modified
+          '''
+        }
+      }
+    }
+
+    stage('Build Docker Image') {
+      steps {
+        container('docker') {
+          sh '''
+            docker build -t \$DOCKERHUB_REPO:latest -f ./nginx-proxy/Dockerfile ./nginx-proxy
+          '''
+        }
+      }
+    }
+
+    stage('Login to DockerHub') {
+      steps {
+        container('docker') {
+          withCredentials([usernamePassword(credentialsId: env.DOCKERHUB_CREDENTIALS, usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+            sh 'echo \$PASS | docker login -u \$USER --password-stdin'
+          }
+        }
+      }
+    }
+
+    stage('Push Image to DockerHub') {
+      steps {
+        container('docker') {
+          sh 'docker push \$DOCKERHUB_REPO:latest'
+        }
+      }
+    }
+  }
+}
+      """.stripIndent())
+      sandbox()
+    }
+  }
+}
+
+pipelineJob('Run_And_Test_Containers') {
+  definition {
+    cps {
+      script("""
+pipeline {
+  agent {
+    kubernetes {
+      yaml '''
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: docker
+    image: docker:20.10.16-dind
+    securityContext:
+      privileged: true
+'''
+    }
+  }
+
+  environment {
+    DOCKERHUB_FLASK = 'maoriiko/flask-app:latest'
+    DOCKERHUB_NGINX = 'maoriiko/nginx-proxy:latest'
+    DOCKERHUB_CREDENTIALS = 'dockerhub-credentials-id'
+  }
+
+  stages {
+    stage('Login to DockerHub') {
+      steps {
+        container('docker') {
+          withCredentials([usernamePassword(credentialsId: env.DOCKERHUB_CREDENTIALS, usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+            sh 'echo \$PASS | docker login -u \$USER --password-stdin'
+          }
+        }
+      }
+    }
+
+    stage('Run Containers') {
 	  steps {
 		container('docker') {
 		  sh '''
@@ -81,16 +227,16 @@ spec:
 		}
 	  }
 	}
-  }
 
-  post {
-    always {
-      container('docker') {
-        sh '''
-          docker stop flask-app || true
-          docker stop nginx-proxy || true
-          docker network rm flask-net || true
-        '''
+    stage('Cleanup') {
+      steps {
+        container('docker') {
+          sh '''
+            docker stop flask-app || true
+            docker stop nginx-proxy || true
+            docker network rm flask-net || true
+          '''
+        }
       }
     }
   }
