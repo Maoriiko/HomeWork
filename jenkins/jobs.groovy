fd711dd12ab5fd7d9d353601a5f33207434a011e
Maoriiko <maorevar@gmail.com>
2025-08-04 17:50:59 +0300

    Changes on jobs.groovy file

--- jenkins/jobs.groovy
+++ jenkins/jobs.groovy
@@ -8,7 +8,7 @@ pipeline {
   stages {
     stage('Checkout') {
       steps {
-        git url: 'https://github.com/yourusername/yourrepo.git', branch: 'main'
+        git url: 'https://github.com/Maoriiko/HomeWork.git', branch: 'main'
       }
     }
     
